e58d6bd3ed73e7c8179b124b8f2fdf9e39257ce2
Kambo2320 <kburke@cougarnet.uh.edu>
2022-10-15 19:27:31 -0500

    updated the proxy handler (experimenting )

--- app/src/main/java/ngavax/app/proxyHandler.java
+++ app/src/main/java/ngavax/app/proxyHandler.java
@@ -2,29 +2,36 @@ package ngavax.app;
 
 import java.net.*;
 import java.io.*;
-import org.json.*;
+//import org.json.*;
 
 public class proxyHandler {
+  public void proxyHandler(){
+    //do nothing
+  }
 
   public void getHTML(String URL) {
     String output = getUrlContents(URL);
     System.out.println(output);
   }
 
-  public String modifyHeader(JSONObject settings, String modifiedheader) {
+  public String modifyHeader(String modifiedheader) { //JSONObject settings
     //This function will modify the header to match the config file
     //This will be done by reading the config file and then modifying the header
     //The config file will have a list of domains and their corresponding IP addresses
     //The header will be modified to match the IP address of the domain
     //This will be done by reading the header and then replacing the domain with the IP address
     //The header will then be returned to the caller
-    JSONObject settings = new JSONObject("{\"serve\":\"http://localserver.tld:80\",\"type\":\"proxy\",\"directory\":\"/\"}"); //slice this up
+    //settings = new JSONObject("{\"serve\":\"http://localserver.tld:80\",\"type\":\"proxy\",\"directory\":\"/\"}"); //slice this up
     String HEADERS = "GET / HTTP/3\nHost: www.tetricz.com\nUser-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:105.0) Gecko/201001\nAccept: text/css,*/*;q=0.1\nAccept-Language: en-US,en;q=0.5\nAccept-Encoding: gzip, deflate, br\nAlt-Used: www.tetricz.com\nConnection: keep-alive\nCookie: _ga=GA1.2.582144703.1664840968\nSec-Fetch-Dest: style\nSec-Fetch-Mode: no-cors\nSec-Fetch-Site: same-origin\nPragma: no-cache\nCache-Control: no-cache\nTE: trailers";
+    String[] arrofHEADERS = HEADERS.split("\n");
 
-    String modifiedheader = ""; //logic to modify header
+    for (String a: arrofHEADERS)
+        System.out.println(a);
 
-    String newHost = settings.getString(key:"serve");
-    String newDirectory = settings.getString(key:"directory")
+    //String modifiedheader = ""; //logic to modify header, 
+
+    String newHost = "localserver.tld:80";
+    String newDirectory = "/";
 
     return modifiedheader;
   }
