be923914e5d2409579d14f84f482b8608cc7ce5f
Tetricz <DavidLeeDaniels@protonmail.com>
2022-11-27 19:26:42 -0600

    CHG: update files

--- dev-tools/dependency.sh
+++ dev-tools/dependency.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo "Installing OpenJDK 18"
-brew install openjdk@18
+echo "Installing OpenJDK 17"
+brew install openjdk@17
 echo "Installing gradle via Homebrew"
 brew install gradle@7
 
-brew cleanup openjdk@18
+brew cleanup openjdk@17
 brew cleanup gradle@7
