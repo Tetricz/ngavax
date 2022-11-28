3ef84eaed83ec9dd1e4fd8ac3237b601eabdfe40
Tetricz <DavidLeeDaniels@protonmail.com>
2022-11-27 19:20:59 -0600

    CHG: move python webservers to own folder

---
+++ Web-Servers-Python/WebServer2.py
@@ -0,0 +1,46 @@
+# Python 3 server example
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
+
+hostName = "localhost"
+serverPort = 8080
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes("<html><head><title>Sample Website</title><style>\
+        body {\
+        font-family: Arial, Helvetica, sans-serif;\
+        margin: 0;\
+        }\
+        .header {\
+        padding: 80px;\
+        text-align: center;\
+        background: #A52A2A;\
+        color: white;\
+        }\
+        .header h1 {\
+        font-size: 40px;\
+        }\
+        </style>\
+        </head>", "utf-8"))
+        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
+        self.wfile.write(bytes("<body>", "utf-8"))
+        self.wfile.write(bytes("<div class=\"header\">", "utf-8"))
+        self.wfile.write(bytes("<h1>Web Server</h1>", "utf-8"))
+        self.wfile.write(bytes("<p>Hello World!</p></div>", "utf-8"))
+        self.wfile.write(bytes("</body></html>", "utf-8"))
+
+if __name__ == "__main__":        
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
